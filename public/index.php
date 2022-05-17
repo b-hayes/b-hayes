64fd1957ffda3d520bade6a71d4a9c098d991300
b-hayes <bradley.hayes@tithe.ly>
2022-05-18 00:32:13 +1000

    htaccess

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 try {
     require_once __DIR__ . '/../vendor/autoload.php';
 
-    echo basename(getcwd()), ' ', $_SERVER['HTTP_HOST'];
+    echo '<pre>', basename(__DIR__) . DIRECTORY_SEPARATOR . basename(__FILE__), '</pre>';
+    echo '<pre>', basename(getcwd()), ' ', $_SERVER['HTTP_HOST'], '</pre>';
 
     $parsedown = new Parsedown();
     echo $parsedown->text(file_get_contents(__DIR__ . '/../README.md'));
