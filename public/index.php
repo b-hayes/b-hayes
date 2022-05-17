3de682cfdd3044dc29d1dbc0e8da7422947aa8db
b-hayes <bradley.hayes@tithe.ly>
2022-05-17 23:35:01 +1000

    htaccess

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 try {
     require_once __DIR__ . '/../vendor/autoload.php';
 
-    echo getcwd();
+    echo basename(getcwd());
 
     $parsedown = new Parsedown();
     echo $parsedown->text(file_get_contents(__DIR__ . '/../README.md'));
