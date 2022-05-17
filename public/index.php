9b6df676b64687daff546e79e205f7e55676d370
b-hayes <bradley.hayes@tithe.ly>
2022-05-17 23:11:55 +1000

    test

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 try {
     require_once __DIR__ . '/../vendor/autoload.php';
 
+    echo getcwd();
+
     $parsedown = new Parsedown();
     echo $parsedown->text(file_get_contents(__DIR__ . '/../README.md'));
 
