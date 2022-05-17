66ea78067975d9d8274b6e672aa2905bd89e41ca
b-hayes <bradley.hayes@tithe.ly>
2022-05-18 00:46:01 +1000

    load css faster

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 try {
     require_once __DIR__ . '/../vendor/autoload.php';
 
-    echo '<pre>', basename(__DIR__) . DIRECTORY_SEPARATOR . basename(__FILE__), '</pre>';
-    echo '<pre>', basename(getcwd()), ' ', $_SERVER['HTTP_HOST'], '</pre>';
-
     $parsedown = new Parsedown();
     echo $parsedown->text(file_get_contents(__DIR__ . '/../README.md'));
 
@@ -15,5 +12,6 @@ try {
 }
 
 ?>
-
-<link rel="stylesheet" type="text/css" href="/css/global.css">
+<style>
+    <?= file_get_contents(__DIR__ . '/css/global.css') ?>
+</style>
