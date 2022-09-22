004239ec0f4b53e8860e33fc54a2b83210bd6282
b-hayes <bradozman@gmail.com>
2022-09-22 16:49:14 +1000

    Better readability

--- public/500.php
+++ public/500.php
@@ -1,17 +1,13 @@
-<?php
-http_response_code(500);
-if ($_SERVER['REQUEST_METHOD'] !== 'GET' || $_SERVER['HTTP_ACCEPT'] === 'application/json') {
-    echo json_encode(['error' => 'internal server error']);
-    return;
-}
-?>
-<div class="middle" style="width: 100%;text-align: center;">
-    <p style="font-size: xxx-large"><?=http_response_code()?></p>
-    <p style="font-size: xxx-large;">🧪💥</p>
-    <h1>Wow that was unexpected!</h1>
+<div class="centered">
+    <div class="middle" style="width: 100%;text-align: center;">
+        <p style="font-size: xxx-large"><?= http_response_code() ?></p>
+        <p style="font-size: xxx-large;">🧪💥</p>
+        <h1>Wow that was unexpected!</h1>
+    </div>
 </div>
 <style>
-    body {
+    .centered {
+        min-height: 50vh;
         display: flex;
         align-items: center; /* Vertical center alignment */
         justify-content: center; /* Horizontal center alignment */
