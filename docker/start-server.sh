5147ae1ae17b75090ed32b215906fe9a216a2b8c
b-hayes <30540495+b-hayes@users.noreply.github.com>
2022-09-25 16:50:34 +1000

    Update docker port to auto assign (#18)

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Lets use the project folder as the name for out docker container.
-PROJECT=`basename $(dirname $(pwd))`
+PROJECT=`basename $(pwd)`
 
-# Empty ports by default lets docker assign any port that is free so we avoid collisions.
-PORTS="8080:80"
+# If port is Zero, a free port is assigned avoiding collisions with other projects.
+PORT="0"
 
 # Run docker
-docker run -dit --name $PROJECT -p $PORTS \
+docker run -dit --name $PROJECT -p $PORT:80 \
   -v `pwd`:/var/www/html \
   -v `pwd`/docker/apache.conf:/etc/apache2/sites-enabled/000-default.conf \
   php:8.1-apache
